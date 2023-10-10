58899bd2b9cf363986cdb7da7101f7a3c2896233
Johannes Przymusinski <j.przymusinski@die-lobby.de>
2023-10-10 20:57:59 +0200

    upgrade test scripts and configs

--- tests/TestBootstrap.php
+++ tests/TestBootstrap.php
@@ -1,15 +1,15 @@
 <?php
 declare(strict_types=1);
-
 /*
  * (c) shopware AG <info@shopware.com>
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-
 use Shopware\Core\TestBootstrapper;
-
-if (is_readable(__DIR__ . '/../vendor/shopware/platform/src/Core/TestBootstrapper.php')) {
+if (is_readable('/opt/share/shopware/tests/TestBootstrapper.php')) {
+    // For Docker image: ghcr.io/friendsofshopware/platform-plugin-dev
+    $testBootstrapper = require '/opt/share/shopware/tests/TestBootstrapper.php';
+} else if (is_readable(__DIR__ . '/../vendor/shopware/platform/src/Core/TestBootstrapper.php')) {
     require __DIR__ . '/../vendor/shopware/platform/src/Core/TestBootstrapper.php';
 } elseif (is_readable(__DIR__ . '/../vendor/shopware/core/TestBootstrapper.php')) {
     require __DIR__ . '/../vendor/shopware/core/TestBootstrapper.php';
@@ -17,12 +17,15 @@ if (is_readable(__DIR__ . '/../vendor/shopware/platform/src/Core/TestBootstrappe
     // vendored from platform, only use local TestBootstrapper if not already defined in platform
     require __DIR__ . '/TestBootstrapper.php';
 }
-
+$autoloadFile = dirname(__DIR__) . '/vendor/autoload.php';
+if (!is_readable($autoloadFile)) {
+    throw new RuntimeException('Could not find autoload.php in vendor/. Did you run composer install?');
+}
+$classLoader = require $autoloadFile;
 return (new TestBootstrapper())
-    ->setProjectDir($_SERVER['PROJECT_ROOT'] ?? dirname(__DIR__, 4))
     ->setLoadEnvFile(true)
-    //->setForceInstallPlugins(true)
-    ->addCallingPlugin()
+    ->setForceInstallPlugins(true)
+    ->addActivePlugins('MobiMamoConnector')
     ->bootstrap()
-    ->setClassLoader(require dirname(__DIR__) . '/vendor/autoload.php')
     ->getClassLoader();
+// docker run --rm -it -v "${PWD}:/plugins/MobiMamoConnector" ghcr.io/friendsofshopware/platform-plugin-dev:v6.4.0 sh -c 'start-mysql && cd /plugins/MobiMamoConnector && phpunit'
