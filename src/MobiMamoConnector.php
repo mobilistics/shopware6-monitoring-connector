9404568024866137001516a401aa7f9756c65712
Johannes Przymusinski <j.przymusinski@die-lobby.de>
2023-10-17 19:39:20 +0200

    :bug: add require_once

--- src/MobiMamoConnector.php
+++ src/MobiMamoConnector.php
@@ -6,6 +6,10 @@ namespace MobilisticsGmbH\MamoConnector;
 
 use Shopware\Core\Framework\Plugin;
 
+if (file_exists(__DIR__ . "/../vendor/autoload.php")) {
+    require_once __DIR__ . "/../vendor/autoload.php";
+}
+
 class MobiMamoConnector extends Plugin
 {
     final public const PLUGIN_IDENTIFIER = 'MobiMamoConnector';
