5fb22aafac1070d419ac54fbbd14971066d55309
Johannes Przymusinski <j.przymusinski@die-lobby.de>
2023-10-24 19:29:00 +0200

    [CHORE] Code styling

--- src/MobiMamoConnector.php
+++ src/MobiMamoConnector.php
@@ -6,8 +6,8 @@ namespace MobilisticsGmbH\MamoConnector;
 
 use Shopware\Core\Framework\Plugin;
 
-if (file_exists(__DIR__ . "/../vendor/autoload.php")) {
-    require_once __DIR__ . "/../vendor/autoload.php";
+if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
+    require_once __DIR__ . '/../vendor/autoload.php';
 }
 
 class MobiMamoConnector extends Plugin
