8e6e956bc2db25cb5d033915e2ec740e68dc33df
Johannes Przymusinski <j.przymusinski@die-lobby.de>
2023-11-07 20:18:46 +0100

    [FIX] The test setup for ci ... finally

--- tests/TestBootstrap.php
+++ tests/TestBootstrap.php
@@ -7,29 +7,24 @@ declare(strict_types=1);
  */
 use Shopware\Core\TestBootstrapper;
 
-$ci = false;
+$projectDir = $_SERVER['SHOPWARE_BUILD_DIR'] ?? null;
+if (!$projectDir) {
+    $projectDir = dirname(__DIR__, 4);
+}
 
-if (is_readable('/opt/share/shopware/tests/TestBootstrapper.php')) {
-    // For Docker image: ghcr.io/friendsofshopware/platform-plugin-dev
-    $testBootstrapper = require '/opt/share/shopware/tests/TestBootstrapper.php';
-    $ci = true;
-} else if (is_readable(__DIR__ . '/../vendor/shopware/platform/src/Core/TestBootstrapper.php')) {
+if (is_readable(__DIR__ . '/../vendor/shopware/platform/src/Core/TestBootstrapper.php')) {
     require __DIR__ . '/../vendor/shopware/platform/src/Core/TestBootstrapper.php';
-} elseif (is_readable(__DIR__ . '/../vendor/shopware/core/TestBootstrapper.php')) {
+} else if (is_readable(__DIR__ . '/../vendor/shopware/core/TestBootstrapper.php')) {
     require __DIR__ . '/../vendor/shopware/core/TestBootstrapper.php';
 } else {
     // vendored from platform, only use local TestBootstrapper if not already defined in platform
     require __DIR__ . '/TestBootstrapper.php';
 }
 
-if ($ci) {
-    $classAutoloader = require '/opt/shopware/vendor/autoload.php';
-} else {
-    $classAutoloader = require dirname(__DIR__) . '/vendor/autoload.php';
-}
+$classAutoloader = require dirname(__DIR__) . '/vendor/autoload.php';
 
 return (new TestBootstrapper())
-    ->setProjectDir($_SERVER['PROJECT_ROOT'] ?? dirname(__DIR__, 4))
+    ->setProjectDir($projectDir)
     ->setLoadEnvFile(true)
     ->setForceInstallPlugins(true)
     ->addActivePlugins('MobiMamoConnector')
